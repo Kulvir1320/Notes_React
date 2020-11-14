a18148dc616aa1a95197281167d4a9a238431179
Alisha <alisha27011997@gmail.com>
2020-11-13 20:40:41 -0500

    New Note file added

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import fire from './config/Fire';
+import Login from './components/LoginRegister';
+import Home from './components/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hii</h1>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount(){
+    this.authListener();
+  }
+
+  authListener(){
+    fire.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({user});
+      }else{
+        this.setState({user:null});
+      }
+    });
+  }
+
+  render(){
+    return (
+        <div>
+          {this.state.user ? (<Home />) : (<Login />)}
+        </div>
+    );
+  }
 }
 
 export default App;
